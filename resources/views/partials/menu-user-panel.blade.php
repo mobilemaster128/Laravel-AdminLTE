c3be3b5bc45a3730c220410163b0788e5e611ebe
DESKTOP-L827G08\Developer <mobilemaster128@gmail.com>
2018-01-27 08:36:19 -0800

    fix storage path

--- resources/views/partials/menu-user-panel.blade.php
+++ resources/views/partials/menu-user-panel.blade.php
@@ -1,5 +1,5 @@
 <div class="pull-left image">
-    <img src="{{ env('APP_URL') . storage_path(Auth::user()[config('adminlte.avatar_key')]) }}" class="img-circle" alt="User Image" />
+    <img src="{{ env('APP_URL') . Storage::url(Auth::user()[config('adminlte.avatar_key')]) }}" class="img-circle" alt="User Image" />
 </div>
 <div class="pull-left info">
     <p style="overflow: hidden;text-overflow: ellipsis;max-width: 160px;" data-toggle="tooltip" title="{{ Auth::user()->name }}">{{ Auth::user()->name }}</p>
