14a040113340bb2147868ffe2363505f5c26d819
DESKTOP-L827G08\Developer <mobilemaster128@gmail.com>
2018-01-28 06:49:29 -0800

    update avatar logic

--- resources/views/partials/menu-user-panel.blade.php
+++ resources/views/partials/menu-user-panel.blade.php
@@ -1,5 +1,10 @@
 <div class="pull-left image">
-    <img src="{{ Storage::url(Auth::user()[config('adminlte.avatar_key')]) }}" class="img-circle" alt="User Image" />
+    <img @if (isset(Auth::user()[config('adminlte.avatar_key')])) 
+        src="{{ Storage::url(Auth::user()[config('adminlte.avatar_key')]) }}"
+        @else 
+        src="{{ asset('vendor/adminlte/img/default.png') }}" 
+        @endif 
+        class="img-circle" alt="User Image" />
 </div>
 <div class="pull-left info">
     <p style="overflow: hidden;text-overflow: ellipsis;max-width: 160px;" data-toggle="tooltip" title="{{ Auth::user()->name }}">{{ Auth::user()->name }}</p>
